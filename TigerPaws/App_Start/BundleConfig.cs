7052c6f34d369150b201403dd9f8fd42bb5a9a53
meanmk <meanmk@users.noreply.github.com>
2018-06-13 02:44:50 +1000

    addedcss

--- TigerPaws/App_Start/BundleConfig.cs
+++ TigerPaws/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace TigerPaws
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-journal3.css",
                       "~/Content/site.css"));
         }
     }
---
+++ TigerPaws/Controllers/ContactController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using TigerPaws.ViewModels;
+
+
+
+namespace TigerPaws.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Index(ContactViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage message = new MailMessage();
+                    message.From = new MailAddress(vm.Email);
+                                                         
+                    message.To.Add("contact@tigerpaws.com");
+                    message.Subject = vm.Subject;
+                    message.Body = vm.Message;
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.tigerpaws.com";
+
+                    smtp.Port = 000;
+
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    ("contact@tigerpaws.com", "password");
+
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(message);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting us ";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View();
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
